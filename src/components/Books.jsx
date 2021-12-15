b3cabde4a53719db7dd763c8c8cd47910ac86c81
RobertaMatera <materaroberta11@gmail.com>
2021-12-15 16:08:33 +0100

    search functionality implemented

--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -1,15 +1,35 @@
 import { Component } from "react";
-import { Row } from "react-bootstrap";
+import { Row, Form } from "react-bootstrap";
 import SingleBook from "./SingleBook";
 class Books extends Component {
-  state = {};
+  state = {
+    searchQuery:""
+  };
+
+
   render() {
     return (
-      <Row>
-        {this.props.books.map((book, i) => (
-          <SingleBook singlebook={book} key={i} />
-        ))}
-      </Row>
+      <>
+        <Row>
+          <Form>        
+            <Form.Group className="mb-3">
+            <Form.Label>Search</Form.Label>
+            <Form.Control type="text" placeholder="Search for Books"
+            value={this.state.searchQuery}
+            onChange = {e => this.setState({searchQuery:e.target.value})}>
+
+            </Form.Control>
+    
+          </Form.Group>
+          </Form>
+
+        </Row>
+        <Row>
+          {this.props.books.filter(book=>book.title.toLowerCase().includes(this.state.searchQuery)).map((book, i) => (
+            <SingleBook singlebook={book} key={i} />
+          ))}
+        </Row>
+      </>
     );
   }
 }
