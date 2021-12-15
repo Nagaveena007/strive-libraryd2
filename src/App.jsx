bd1ee8212a1eb0a2f9c7bef5d293668321f70713
RobertaMatera <materaroberta11@gmail.com>
2021-12-15 15:22:25 +0100

    badge + single book card

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,9 @@ import Home from "./components/Home";
 import FooterPart from "./components/FooterPart";
 import JumbotronPart from "./components/JumbotronPart";
 import WarningSign from "./components/WarningSign";
+import MyBadge from "./components/MyBadge";
+import SingleBook from "./components/SingleBook";
+import scifi from "./data/scifi.json"
 
 function App() {
   return (
@@ -13,6 +16,9 @@ function App() {
       <MyNav />
       <JumbotronPart />
       <WarningSign text="This is a danger alert!" />
+      <MyBadge text="Hey I am Badge" color="danger"/>
+      <SingleBook singlebook={scifi[0]}/>
+      
       <Home  />
       
       <FooterPart />
